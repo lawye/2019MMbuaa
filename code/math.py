d2b9f3e8d86719a146f2e4294bd1a32926ee8c3a
lawye <lawyepc@qq.com>
2019-07-27 19:44:55 +0800

    先更新一下

--- code/math.py
+++ code/math.py
@@ -1,13 +1,32 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
-data = pd.read_csv('./DATA/RAW/GSE81089/FPKM_cufflinks.tsv', sep='\t',header =None)
-data_T = data.T
-data_T = data_T.drop(0,axis=1)
-data_T_columns = data_T[0:1]
-data_T =  data_T.drop(0,axis=0)
-data_T.columns=data_T_columns.values.tolist()
-data_T = data_T.reset_index(drop=True)
-data_T=data_T.astype(float)
-data_T.corr()
+import networkx as nx
+jishu=0
+G=nx.Graph()
+data = pd.read_csv('./FPKM_cufflinks.tsv', sep='\t')
+index = data["Ensembl_gene_id"]
+data= data.drop("Ensembl_gene_id",axis=1)
+data = data.dropna(axis=0)
+edge_color = list()
+for i in range(data.shape[0]):
+    for j in range(i+1,data.shape[0]):
+        x = data[i:i+1]
+        y = data[j:j+1]
+        t = np.corrcoef(x,y)
+        print(jishu)
+        if t[0][1] >0.85 :
+            G.add_edge(i,j)
+            edge_color.append("red")
+            pos = nx.circular_layout(G)
+            nx.draw(G,pos,with_labels=True,node_size = 50,node_color = "blue",edge_color = edge_color)
+            jishu+=1
+            if jishu%100==0 : plt.show()
+        elif t[0][1] < -0.85 :
+            G.add_edge(i,j)
+            edge_color.append("blue")
+            pos = nx.circular_layout(G)
+            nx.draw(G,pos,with_labels=True,node_size = 50,node_color = "blue",edge_color = edge_color)
+            jishu+=1
+            if jishu%100==0 : plt.show()
+plt.show()
